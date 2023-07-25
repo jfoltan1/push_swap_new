8f7a0c4f0718c3302824b4c052cbd5c86a118358
Julius Foltan <jfoltan@4c0c1.42berlin.de>
2023-07-25 17:37:31 +0200

    sort 3 loses a node

--- test_operations/swappers.c
+++ test_operations/swappers.c
@@ -9,6 +9,7 @@ t_stack *stack_last(t_stack *lst)
         lst = lst->next;
     return (lst);
 }
+
 void    stack_add_back(t_stack *lst, int content,int index)
 {
     t_stack *old;
@@ -39,6 +40,7 @@ void	print_stack (t_stack *stack)
 	ft_printf("%d ", stack->val);
 	ft_printf("\n");
 }
+
 void	print_stack_index (t_stack *stack)
 {
 	while (stack -> next != NULL)
@@ -50,8 +52,9 @@ void	print_stack_index (t_stack *stack)
 	}
 	ft_printf("%d ", stack->val);
 	ft_printf("%d ", stack->index);
-	ft_printf("\n");
+	ft_printf("\n\n");
 }
+
 t_stack *parse_stack(char **av, int ac)
 {
     int     i;
@@ -71,23 +74,6 @@ t_stack *parse_stack(char **av, int ac)
     return (stack);
 }
 
-int rra (t_stack **a_stack)
-{
-	t_stack	*last_node;
-	t_stack *second_last_node;
-
-	last_node = *a_stack;
-	second_last_node = NULL;
-	while (last_node -> next != NULL)		
-	{
-		second_last_node = last_node;
-		last_node = last_node -> next;
-	}
-	last_node -> next = *a_stack;
-	*a_stack = last_node;
-	second_last_node -> next = NULL;
-    return 1;
-}
 int is_sorted(t_stack *stack)
 {
 	while (stack -> next != NULL)
@@ -96,11 +82,11 @@ int is_sorted(t_stack *stack)
 			stack = stack -> next;
 		else 
 			{
-				ft_printf("Stack is nicht sorted!");
+				ft_printf("Stack is nicht sorted!\n");
 				return (0);
 			}
 	}
-	ft_printf("Stack is sorted!");
+	ft_printf("Stack is sorted!\n");
 	return (1);
 }
 
@@ -113,7 +99,6 @@ void index_stack(t_stack *stack, int ac)
     i = 0;
     head = stack;
     swap = INT_MAX; 
-    stack = head;
     while (i < ac)
     {
         while (stack != NULL)
@@ -133,29 +118,112 @@ void index_stack(t_stack *stack, int ac)
         stack = head;
         i++;
     }
+	stack =	head;
+}
+int ra(t_stack *a_stack)
+{
+    int first_val; 
+    int first_index;
+	first_val = a_stack->val;
+	first_index = a_stack->index;
+
+        while (a_stack->next != NULL)
+    {
+        a_stack->val = a_stack->next->val;
+        a_stack->index = a_stack->next->index;
+        a_stack = a_stack->next;
+    }
+    a_stack->val = first_val;
+    a_stack->index = first_index;
+    a_stack->next = NULL;
+
+    return 1;
 }
 
+int rra (t_stack **a_stack)
+{
+	t_stack	*last_node;
+	t_stack *second_last_node;
+
+	last_node = *a_stack;
+	second_last_node = NULL;
+	while (last_node -> next != NULL)		
+	{
+		second_last_node = last_node;
+		last_node = last_node -> next;
+	}
+	last_node -> next = *a_stack;
+	*a_stack = last_node;
+	second_last_node -> next = NULL;
+    return 1;
+}
+
+int	sa(t_stack *a_stack)
+{
+	int swap;
+	t_stack *head;
+	head = a_stack;
+	if (a_stack -> next == NULL)
+		return(0);
+	swap = a_stack -> val;
+	a_stack -> val = a_stack -> next -> val;
+	a_stack -> next -> val = swap;
+	/* do  I need to free swap?  */
+	a_stack = head;
+	return(1);
+}
+int	sort_3(t_stack *stack)
+{
+	int	*pos;
+	int	i;
+	t_stack	*head;
+
+	if (!stack)
+		return (-1);
+	i = 0;
+	head = stack;
+	pos = malloc(3 * sizeof(int));
+	while (stack -> next != NULL)
+	{
+		pos[i] = stack -> index;
+		stack = stack->next;
+		i++;
+	}
+	pos[i] = stack -> index;
+	stack = head;
+	if (pos[0] < pos[1] && pos[1] < pos[2])
+		return (0);
+	else if (pos[0] > pos[1] && pos[1] < pos[2] && pos[2] > pos[0])
+		rra(&stack);
+	else if (pos[0] > pos[1] && pos[1] > pos[2])
+	{
+		sa(stack);
+		rra(&stack);
+	}
+	else if (pos[0] > pos[1] && pos[1] < pos[2] && pos[2] < pos[0])
+		ra(stack);
+	else if (pos[0] < pos[1] && pos[1] < pos[2] && pos[2] > pos[0])
+	{
+		sa(stack);
+		ra(stack);
+	}
+	else if (pos[0] < pos[1] && pos[1] > pos[2] && pos[2] < pos[0])
+		rra(&stack);
+	return (0);
+}
 int main(int ac, char **av)
 {
     t_stack *a_stack;
- 	//t_stack *b_stack;
-	//char		**bv;
-
-	//bv = ft_calloc(4,sizeof(char **));
-	//bv[1] = "10";
-	//bv[2] = "11";
-	//bv[3] = "9";
+ 
+	
     a_stack = parse_stack(av, ac);
-	//b_stack = parse_stack(bv, ac);
+
 	index_stack(a_stack,ac);
 	print_stack_index(a_stack);
 	//rra(&a_stack);
-	//print_stack_index(a_stack);
-	is_sorted(a_stack);
-	//print_stack(b_stack);
-	//ra(a_stack);
-	//print_stack(a_stack);
-	//print_stack(b_stack);	
+	sort_3(a_stack);
 
+	print_stack_index(a_stack);
+	
 	return(0);
 }
